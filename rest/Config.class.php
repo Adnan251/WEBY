e4c73a6dbb9802d5c9aa5ba6fbccb372360cd7fe
Adnan251 <adnan.dzindo@stu.ibu.edu.ba>
2022-09-15 13:18:39 +0200

    herokuuuu yay

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -3,16 +3,16 @@
 class Config {
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "localhost");
+    return Config::get_env("DB_HOST", "eu-cdbr-west-03.cleardb.net");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "AdnanDzindo");
+    return Config::get_env("DB_USERNAME", "bdd1717f590982");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "SQLAdnan251");
+    return Config::get_env("DB_PASSWORD", "7f7b4e6e");
   }
   public static function DB_SCHEME(){
-    return Config::get_env("DB_SCHEME", "weby_db");
+    return Config::get_env("DB_SCHEME", "heroku_671e02bc8690d67");
   }
   public static function DB_PORT(){
     return Config::get_env("DB_PORT", "3306");
